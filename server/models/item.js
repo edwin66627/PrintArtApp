4eeec7991e23ec3602a2e79621b9eff5c3221b20
Edwin Quevedo <edwinqs27@gmail.com>
2020-10-11 17:29:07 -0500

    Mongoose installed and User Model added

---
+++ server/models/item.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const ItemSchema = mongoose.Schema({
+    title: { type: String, required: true, trim: true },
+    artist: { type: String, trim: true },
+    image: { type: String, trim: true },
+    year: { type: Number },
+    price: { type: Number },
+})
+
+module.exports = mongoose.model('Item', ItemSchema)
