ddd988bd011ea06a5f5332938446d7cda78bbba2
Edwin Quevedo <edwinqs27@gmail.com>
2020-10-11 15:43:01 -0500

    Route 'items' set up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use('/items', require('./routes/items'))
+
 const port = 8070;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
