8aa4f81317c604b3bf846260a463f8ea585bebcb
Edwin Quevedo <edwinqs27@gmail.com>
2020-10-11 20:24:28 -0500

    body-parser installed and Item post request set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use('/items', require('./routes/items'))
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/printart', {
   useNewUrlParser: true, useCreateIndex: true
 });
